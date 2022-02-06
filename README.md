70a18a89098f2ac80f3d3abad7e1fc5790ce775e
Min-Haw, Liu <liuminhaw@gmail.com>
2022-02-07 01:12:40 +0800

    init: separate to independent repository for version 0.1.0

---
+++ README.md
@@ -0,0 +1,52 @@
+# restic-bkp
+Backup files with custom configuration using restic
+
+## Pre-requisite
+restic need to be installed before using this script  
+**reference:** [Installation](https://restic.readthedocs.io/en/latest/020_installation.html)
+
+## Version
+### v0.1.0
+- Use new config format: json
+    - Support multiple sources and destinations configuration
+- New configuration setting: forget policy
+- Moving from [some-script](https://github.com/liuminhaw/some-scripts) repositroy to separate independent repository
+
+**More versions information:** [Versions Documentation](https://mewing-pisces-b2c.notion.site/restic-bkp-1b852fb0215a4e1d8df00081f3050e1b) 
+
+## Setup
+#### Configuration
+Set backup information (source, destination, password, exclusion)
+
+#### Usage
+```
+Usage: restic-bkp.sh [--help] [--version] [--type=local|sftp] backup|init|snapshots
+    --help                      Display this help message and exit
+    --type=[local|sftp]         
+    --type [local|sftp]         Specify backup destination type: (local, sftp)
+                                Default type: local
+    --version                   Show version information
+    action                      Command to execute: (backup, init, snapshots)
+                                backup: create new backup snapshot
+                                init: prepare backup destination directory
+                                snapshots: list previous snapshots
+```
+
+#### Exclusion
+The `exclude_file` key in config file (default: excludes.txt)<br>
+Create the file with listed files and directories for exclusion
+```
+List
+excluded 
+files
+and
+diretories
+here
+``` 
+
+#### Password
+The `password_file` key in config file (default: .restic.pass)<br>
+Password is needed when restic process
+```
+defaultPasswordToEncryptResticSnapshot
+```
---
+++ restic-bkp.sh
@@ -0,0 +1,160 @@
+#!/bin/bash
+#
+# Backup files with custom configuration using restic
+
+
+# Script var
+_VERSION=0.1.0
+
+# ----------------------------------------------------------------------------
+# Show script usage
+# Outputs:
+#   Write usage information to stdout
+# ----------------------------------------------------------------------------
+show_help() {
+cat << EOF
+Usage: ${0##*/} [--help] [--version] [--type=local|sftp] backup|init|snapshots
+    --help                      Display this help message and exit
+    --type=[local|sftp]         
+    --type [local|sftp]         Specify backup destination type: (local, sftp)
+                                Default type: local
+    --version                   Show version information
+    action                      Command to execute: (backup, init, snapshots)
+                                backup: create new backup snapshot
+                                init: prepare backup destination directory
+                                snapshots: list previous snapshots
+EOF
+}
+
+_config="config.json"
+_backup_type="local"
+
+
+# Command line options
+while :; do
+    case ${1} in
+        --help)
+            show_help
+            exit
+            ;;
+        --version)
+            echo "Version: ${_VERSION}"
+            exit
+            ;;
+        --type)
+            if [[ "${2}" ]]; then
+                _backup_type=${2}
+                shift
+            else
+                echo -e "[ERROR] '--type' requires a non-empty option argument." 1>&2
+                exit 1
+            fi
+            ;;
+        --type=?*)
+            _backup_type=${1#*=} # Delete everything up to "=" and assign the remainder
+            ;;
+        --type=)
+            echo -e "[ERROR] '--type' requires a non-empty option argument." 1>&2
+            exit 1
+            ;;
+        -?*)
+            echo -e "[WARN] Unknown option (ignored): ${1}" 1>&2
+            exit 1
+            ;;
+        *)  # Default case: no more options
+            break
+    esac
+
+    shift
+done
+
+if [[ ${#} != 1 ]]; then
+    show_help
+    exit 1
+fi
+_action=${1}
+
+if [[ "${_action}" != "backup" && "${_action}" != "init" && "${_action}" != "snapshots" ]]; then
+    show_help
+    exit 1
+fi
+
+if [[ ! -z "${_backup_type}" && "${_backup_type}" -ne "local" && "${_backup_type}" -ne "sftp" ]]; then
+    show_help
+    exit 1
+fi
+
+if [[ ! -f "${_config}" ]]; then
+    echo "[ERROR] config file: ${_config} not found"
+    exit 1
+fi
+
+_password_file=$(jq -r .password_file ${_config})
+_exclude_file=$(jq -r .exclude_file ${_config})
+
+if [[ ! -f "${_password_file}" ]]; then
+    echo "[ERROR] secret password file: ${_password_file} not found"
+    exit 1
+fi
+
+if [[ ! -f "${_exclude_file}" ]]; then
+    echo "[ERROR] exclude content file: ${_exclude_file} not found"
+    exit 1
+fi
+
+if [[ "${_backup_type}" == "local" ]]; then
+    _local=$(jq -r .local ${_config})
+    _local_len=$(jq length <<< ${_local})
+    for (( i=0; i<${_local_len}; i++ )); do
+        _local_src=$(jq -r --arg i "${i}" '.local[$i|tonumber].src' ${_config})
+        _local_dest=$(jq -r --arg i "${i}" '.local[$i|tonumber].dest' ${_config})
+        _dest_repos[${i}]="${_local_dest}"
+        _src_repos[${i}]="${_local_src}"
+    done
+elif [[ "${_backup_type}" == "sftp" ]]; then
+    _sftp=$(jq -r .sftp ${_config})
+    _sftp_len=$(jq length <<< ${_sftp})
+    for (( i=0; i<${_sftp_len}; i++ )); do
+        _sftp_host=$(jq -r --arg i "${i}" '.sftp[$i|tonumber].host' ${_config})
+        _sftp_src=$(jq -r --arg i "${i}" '.sftp[$i|tonumber].src' ${_config})
+        _sftp_dest=$(jq -r --arg i "${i}" '.sftp[$i|tonumber].dest' ${_config})
+        _dest_repos[${i}]="sftp:${_sftp_host}:${_sftp_dest}"
+        _src_repos[${i}]="${_sftp_src}"
+    done
+else
+    echo "[ERROR] invalid type parameter: ${_backup_type}"
+    exit 1
+fi
+
+# TODO: Check for empty config value (host / src / dest)
+
+echo "Backup type: ${_backup_type}"
+echo "Secret password file: ${_password_file}"
+echo "Exclude content file: ${_exclude_file}"
+echo ""
+
+case ${_action} in
+    init)
+        for (( i=0; i<${#_dest_repos[@]}; i++ )); do
+            echo "[INFO] Destination: ${_dest_repos[${i}]}"
+            restic init -r ${_dest_repos[${i}]} --password-file ${_password_file}
+            echo ""
+        done
+        ;;
+    snapshots)
+        for (( i=0; i<${#_dest_repos[@]}; i++ )); do
+            echo "[INFO] Destination: ${_dest_repos[${i}]}"
+            restic snapshots -r ${_dest_repos[${i}]} --password-file ${_password_file}
+            echo ""
+        done
+        ;;
+    backup)
+        for (( i=0; i<${#_src_repos[@]}; i++ )); do
+            echo "[INFO] Source: ${_src_repos[${i}]}, Destination: ${_dest_repos[${i}]}"
+            restic backup -v -r ${_dest_repos[${i}]} --exclude-file="${_exclude_file}" --password-file ${_password_file} ${_src_repos[${i}]}
+            restic prune -r ${_dest_repos[${i}]} --password-file ${_password_file}
+            echo ""
+        done
+        ;;
+esac
+
