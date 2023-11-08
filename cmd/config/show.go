96d89198ee8c8a5e022f48ccecbb2d079a9be5ec
Min-Haw, Liu <liuminhaw@gmail.com>
2023-11-09 01:50:34 +0800

    feat: init config file setup

--- cmd/config/show.go
+++ cmd/config/show.go
@@ -19,15 +19,20 @@ LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
 OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
 THE SOFTWARE.
 */
-package cmd
+package config
 
 import (
 	"errors"
 	"fmt"
+	"log"
 	"os"
 	"os/exec"
+	"strings"
 
+	"github.com/liuminhaw/wrestic-bkp/restic"
 	"github.com/spf13/cobra"
+	"github.com/spf13/viper"
+	"gopkg.in/yaml.v3"
 )
 
 var (
@@ -35,21 +40,64 @@ var (
 )
 
 // checkCmd represents the check command
-var checkCmd = &cobra.Command{
-	Use:   "check",
-	Short: "check for prerequisite",
+var showCmd = &cobra.Command{
+	Use:   "show [backup name]",
+	Short: "show configuration",
 	Long:  ``,
+	Args: func(cmd *cobra.Command, args []string) error {
+		// Optionally run one of the validators provided by cobra
+		if err := cobra.MaximumNArgs(1)(cmd, args); err != nil {
+			return err
+		}
+		// Run the custom validation logic
+		if len(args) == 0 {
+			return nil
+		}
+
+		config, err := restic.NewConfig(viper.ConfigFileUsed())
+		if err != nil {
+			return fmt.Errorf("load config: %w", err)
+		}
+		if isValidConfigName(config, args[0]) {
+			return nil
+		}
+
+		return fmt.Errorf("given name '%s' not found in config backup names: %v", args[0], validConfigNames(config))
+	},
 	Run: func(cmd *cobra.Command, args []string) {
+		var backupName string
+		if len(args) == 1 {
+			backupName = args[0]
+		}
+
 		err := CmdCheck("restic")
 		if err != nil {
 			fmt.Println("restic should be installed before using this program")
 			os.Exit(1)
 		}
+
+		backups, err := restic.NewConfig(viper.ConfigFileUsed())
+		if err != nil {
+			log.Fatalf("check: %v\n", err)
+		}
+		// Now you can use the config struct, for example, print the backup names
+		for _, backup := range backups.Backups {
+			data, err := yaml.Marshal(backup)
+			if err != nil {
+				log.Fatalf("config check: %v\n", err)
+			}
+			if backupName == "" || backupName == backup.Name {
+				fmt.Printf("--- config backup:\n%s\n\n", string(data))
+			}
+			// fmt.Printf("Name: %s\n", backup.Name)
+			// fmt.Printf("Type: %s\n", backup.Type)
+			// fmt.Println(backup.Config.String())
+		}
 	},
 }
 
 func init() {
-	rootCmd.AddCommand(checkCmd)
+	ConfigCmd.AddCommand(showCmd)
 
 	// Here you will define your flags and configuration settings.
 
@@ -70,4 +118,23 @@ func CmdCheck(command string) error {
 	}
 
 	return nil
+}
+
+func isValidConfigName(config *restic.Config, name string) bool {
+	for _, backup := range config.Backups {
+		if backup.Name == name {
+			return true
+		}
+	}
+	return false
+}
+
+func validConfigNames(config *restic.Config) string {
+	var builder strings.Builder
+	builder.WriteString("[ ")
+	for _, backup := range config.Backups {
+		builder.WriteString(fmt.Sprintf("'%s' ", backup.Name))
+	}
+	builder.WriteString("]")
+	return builder.String()
 }
