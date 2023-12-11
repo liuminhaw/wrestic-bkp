004fda3d71b19c678a88b8470045ec40d56511ea
Min-Haw, Liu <liuminhaw@gmail.com>
2023-12-11 23:24:46 +0800

    feat: add exclude config when backing up

--- restic/repository.go
+++ restic/repository.go
@@ -22,6 +22,7 @@ type LocalBackupRepository struct {
 	Password    string
 	Destination string
 	Sources     []string
+	Excludes    []string
 }
 
 func (r LocalBackupRepository) Init() ([]byte, error) {
@@ -46,9 +47,20 @@ func (r LocalBackupRepository) Backup() error {
 	commandArg := []string{"backup", "-r", r.Destination}
 	commandArg = append(commandArg, r.Sources...)
 
+	// Add exclude option
+	for _, exclude := range r.Excludes {
+		excludeOpt := fmt.Sprintf("--exclude=%s", exclude)
+		commandArg = append(commandArg, excludeOpt)
+	}
+
+	// commandArg = append(commandArg, "--dry-run", "-vv")
+
 	var stderr bytes.Buffer
 	cmd := exec.Command("restic", commandArg...)
 	cmd.Stderr = &stderr
+
+	// fmt.Printf("Command path: %s\n", cmd.Path)
+	// fmt.Printf("Command args: %+v\n", cmd.Args)
 
 	stdout, err := cmd.StdoutPipe()
 	if err != nil {
