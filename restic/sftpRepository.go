3f67117316323ea3a982e4c642c028c7b8569a15
Min-Haw, Liu <liuminhaw@gmail.com>
2023-12-27 23:22:50 +0800

    feat: sftp type repository backup and snapshots

--- restic/sftpRepository.go
+++ restic/sftpRepository.go
@@ -8,6 +8,7 @@ import (
 	"io/fs"
 	"os"
 	"os/exec"
+	"regexp"
 	"strings"
 )
 
@@ -65,11 +66,92 @@ func (r SftpBackupRepository) Init() ([]byte, error) {
 }
 
 func (r SftpBackupRepository) Backup() error {
+	os.Setenv(passwordEnv, r.Password)
+	os.Setenv(resticProgressFPS, resticProgressFPSValue)
+
+	// Check if ConfigHost setting exist in ssh config file
+	foundHost, err := checkSshHost(r.ConfigHost)
+	if err != nil {
+		return fmt.Errorf("sftpBackupRepository snapshots: %w", err)
+	}
+	if !foundHost {
+		fmt.Print(sshConfigSetupMsg)
+		return fmt.Errorf("host setting %s not found in ssh config file", r.ConfigHost)
+	}
+
+	commandArg := []string{"backup", "-r", fmt.Sprintf("sftp:%s:%s", r.ConfigHost, r.Destination)}
+	commandArg = append(commandArg, r.Sources...)
+
+	// Add exclude option
+	for _, exclude := range r.Excludes {
+		excludeOpt := fmt.Sprintf("--exclude=%s", exclude)
+		commandArg = append(commandArg, excludeOpt)
+	}
+
+	var stderr bytes.Buffer
+	cmd := exec.Command("restic", commandArg...)
+	cmd.Stderr = &stderr
+
+	stdout, err := cmd.StdoutPipe()
+	if err != nil {
+		return fmt.Errorf("sftpBackupRepository backup: %w", err)
+	}
+
+	// Start command
+	if err := cmd.Start(); err != nil {
+		return fmt.Errorf("sftpBackupRepository backup: command start: %w", err)
+	}
+
+	// Read from the pipe
+	linesCount := 0
+	scanner := bufio.NewScanner(stdout)
+	for scanner.Scan() {
+		str := scanner.Text()
+		// Check if string start with pattern `[x:xx]`
+		pattern := regexp.MustCompile(`^\[\d+:\d\d\]`)
+		if pattern.MatchString(str) {
+			for i := 0; i < linesCount; i++ {
+				fmt.Print("\033[A")
+			}
+			fmt.Print("\033[K")
+			linesCount = 0
+		}
+		fmt.Println(str) // Print each line of output
+		linesCount++
+	}
+
+	// Wait for the command to finish
+	if err := cmd.Wait(); err != nil {
+		fmt.Println(stderr.String())
+		return fmt.Errorf("sftpBackupRepository backup: command wait: %w", err)
+	}
+
 	return nil
 }
 
 func (r SftpBackupRepository) Snapshots() ([]byte, error) {
-	return nil, nil
+	os.Setenv(passwordEnv, r.Password)
+
+	// Check if ConfigHost setting exist in ssh config file
+	foundHost, err := checkSshHost(r.ConfigHost)
+	if err != nil {
+		return nil, fmt.Errorf("sftpBackupRepository snapshots: %w", err)
+	}
+	if !foundHost {
+		fmt.Print(sshConfigSetupMsg)
+		return nil, fmt.Errorf("host setting %s not found in ssh config file", r.ConfigHost)
+	}
+
+	var stderr bytes.Buffer
+	commandArg := []string{"snapshots", "-r", fmt.Sprintf("sftp:%s:%s", r.ConfigHost, r.Destination)}
+	cmd := exec.Command("restic", commandArg...)
+	cmd.Stderr = &stderr
+	output, err := cmd.Output()
+	if err != nil {
+		return stderr.Bytes(), fmt.Errorf("sftpBackupRepository snapshots: %w", err)
+	}
+
+	return output, nil
 }
 
 // checkSshHost find if configHost is set in user's ssh config file with syntax 'Host configHost'
